23c5713ebc717dee0c4a97517da5eb277e5cfed7
Dominik Ritz <dom_ritz@icloud.com>
2026-07-31 11:31:15 -0400

    Move site hosting to Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dygitz.github.io',
+  site: 'https://dominikritz.com',
   integrations: [svelte()],
   vite: {
     plugins: [tailwindcss()]
