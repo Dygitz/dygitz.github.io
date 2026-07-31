d914d5159b65963cb4087b6e7a3d274828308354
Dominik Ritz <dom_ritz@icloud.com>
2026-07-30 23:50:07 -0400

    feat: migrate to Tailwind CSS v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dygitz.github.io',
-  integrations: [svelte()]
+  integrations: [svelte()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
